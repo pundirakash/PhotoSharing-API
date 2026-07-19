76881b6e6a065b81bdc55a2dba76334052b7319d
Akash Pundir <pundirakash@outlook.com>
2026-07-19 19:16:00 +0530

    Added Vercel.json

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express=require('express');
 require('dotenv').config();
+const cors=require('cors');
 const postRoutes=require("./src/routes/postRoutes")
 const userRoutes=require("./src/routes/userRoutes")
 const logger=require("./src/middlewares/logger")
 const errorHandler=require("./src/middlewares/errorHandler");
 const connectDB=require('./src/config/db');
 
+
 connectDB();
 
 const app=express();
-
+app.use(cors());
 //Inbuilt Middleware
 app.use(express.json());
 //Application level middleware
@@ -25,3 +27,5 @@ app.use(errorHandler);
 app.listen(3000,()=>{
     console.log("Server listening on port 3000");
 })
+
+module.exports=app;
